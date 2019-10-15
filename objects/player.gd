5b7aedfd5687cdca184c9251d5cc39fe6aa8ae08
32BitFlame <47258322+32BitFlame@users.noreply.github.com>
2019-10-15 18:29:05 -0400

    Update player.gd

--- objects/player.gd
+++ objects/player.gd
@@ -4,27 +4,31 @@ export (float) var walk_speed;
 export (float) var sprint_speed;
 export (float) var tired_speed;
 
+export (int) var sprint_timer;
+export (int) var sprint_timer_diff;
+
+var sprint_timer_iter = 0;
+var sprint_cooldown = true;
+
 var move_speed = walk_speed;
 var velocity = Vector2();
 var move_vector_normalized = Vector2(0,0);
 
-export (int) var sprint_timer;
-var sprint_timer_iter = 0;
-export (int) var sprint_timer_diff;
-var sprint_cooldown = true;
-export (int) var net_iter_max;
 func _ready():
 	pass
 
 func weapon_fire():
-	#Fire
+	# Fire
 	if Input.is_action_just_pressed("fire"):
 		print_debug()
+
 func _physics_process(delta):
-	look_at(get_viewport().get_mouse_position());
+	# Rotates to face player
+	look_at(get_global_mouse_position());
 	rotate(1.57079633);
-	move_vector_normalized = Vector2(0,0);
 	
+	
+	move_vector_normalized = Vector2(0,0);
 	
 	#Sprint: Checks if player is able to sprint and the key is pressed
 	if Input.is_action_pressed("sprint") && sprint_timer_iter < sprint_timer && !sprint_cooldown:
@@ -41,14 +45,14 @@ func _physics_process(delta):
 			sprint_cooldown = false;
 	
 	#Checks for button presses and sets movement vector accordingly
-	if Input.is_action_pressed("ui_right"):
-		move_vector_normalized.x += 1;
-	if Input.is_action_pressed("ui_left"):
-		move_vector_normalized.x -= 1;
-	if Input.is_action_pressed("ui_up"):
-		move_vector_normalized.y -= 1;
-	if Input.is_action_pressed("ui_down"):
-		move_vector_normalized.y += 1;
+	move_vector_normalized.x += _castBoolToInt(Input.is_action_pressed("ui_right"));
+	move_vector_normalized.x -= _castBoolToInt(Input.is_action_pressed("ui_left"));
+	
+	move_vector_normalized.y -= _castBoolToInt(Input.is_action_pressed("ui_up"));
+	move_vector_normalized.y += _castBoolToInt(Input.is_action_pressed("ui_down"));
 	
 	#Moves and collides with movement vector with a magnitude of movement speed
 	move_and_slide(move_vector_normalized * move_speed, Vector2(0,0));
+
+func _castBoolToInt(boolVal):
+	return 1 if boolVal else 0
