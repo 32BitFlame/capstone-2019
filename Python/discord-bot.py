e514992ef86dd19bf5603765db3d4269b8d5f67f
BuildTools <toy.fighter1@gmail.com>
2019-10-21 20:33:00 -0400

    First attempt at Discord bot server code.

--- Python/discord-bot.py
+++ Python/discord-bot.py
@@ -1,12 +1,31 @@
+from aiohttp import web
 import discord
 from discord.ext import commands
 
 
+bot = commands.Bot(command_prefix='!')
+routes = web.RouteTableDef()
+to_send = None
+
+@routes.get('/')
+async def enemy(request):
+    return web.Response(text=to_send)
+
+
+@bot.event
+async def on_ready():
+    app = web.Application()
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, 'localhost', 8080)
+    await site.start()
+
+
 @commands.command()
 async def addenemy(ctx, enemy: str):
-    await ctx.send("Test.")
+    to_send = enemy
+    await ctx.send("Sent away!.")
 
-bot = commands.Bot(command_prefix='-')
 
 bot.run('TOKEN')
 
