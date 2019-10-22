d4ee47bc0203780c52b8a58c4830e882e017b2e9
doctormacc <toy.fighter1@gmail.com>
2019-10-22 18:04:46 -0400

    Server implementation that actually works. TODO: Change on_ready() behavior as that will update every time the Discord connection changes.

--- Python/discord-bot.py
+++ Python/discord-bot.py
@@ -1,31 +1,38 @@
 from aiohttp import web
 import discord
 from discord.ext import commands
-
+import token
 
 bot = commands.Bot(command_prefix='!')
-routes = web.RouteTableDef()
-to_send = None
-
-@routes.get('/')
-async def enemy(request):
-    return web.Response(text=to_send)
 
 
-@bot.event
-async def on_ready():
-    app = web.Application()
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, 'localhost', 8080)
-    await site.start()
+class DiscordBot(commands.Cog):
+
+    def __init__(self):
+        self.to_send = None
+        self.site = None
+
+    async def enemy(self, request):
+        res = {'enemy': self.to_send}
+        return web.json_response(res)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        app = web.Application()
+        app.add_routes([web.get('/', self.enemy)])
+        runner = web.AppRunner(app)
+        await runner.setup()
+        self.site = web.TCPSite(runner, 'localhost', 8080)
+        await self.site.start()
+
+    @commands.command()
+    async def addenemy(self, ctx, enemy: str):
+        self.to_send = enemy
+        await self.site.stop()
+        await self.site.start()
+        await ctx.send("Sent away!")
 
 
-@commands.command()
-async def addenemy(ctx, enemy: str):
-    to_send = enemy
-    await ctx.send("Sent away!.")
-
-
-bot.run('TOKEN')
+bot.add_cog(DiscordBot())
+bot.run('token')
 
