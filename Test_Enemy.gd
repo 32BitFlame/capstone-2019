4b0ad2b84b84741ff44682fc83838d375749d581
PyroBrine <aidenjlalka@gmail.com>
2019-11-19 17:30:29 -0500

    Commit 1

--- Test_Enemy.gd
+++ Test_Enemy.gd
@@ -16,23 +16,29 @@ var Volacity = Vector2()
 var V_Normalized = Vector2()
 var Direction_Change_CD = 0
 
+var Player_Location = Vector2()
+var Player
+
 var Colliding = false
 var Player_Detect = false
+
+func _ready():
+	Player = get_parent().get_node("Player")
 
 #Enemy AI
 func _physics_process(delta):
 	#Determines how enemy will act in current situation.
 	if Player_Detect == true:
-		Volacity = Vector2()
+		Player_Location = Vector2(Player.position.x, Player.position.y)
 		
-		Volacity = Vector2(Walk_Speed, 0).rotated(self.get_angle_to(Vector2(0, 0)))
+		Volacity = Vector2()
+		Volacity = Vector2(Walk_Speed, 0).rotated(self.get_angle_to(Player_Location))
 	elif !Colliding && Player_Detect == false:
 		if Direction_Change_CD == 0:
 			rotation = rand_range(0, 360)
 			Direction_Change_CD = int(rand_range(Lower_DC_CD_Range, Upper_DC_CD_Range))
 		
 		Volacity = Vector2()
-		
 		Volacity = Vector2(Walk_Speed, 0).rotated(rotation)
 		
 		Direction_Change_CD -= 1
