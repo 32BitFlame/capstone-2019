a700dae38a6d461eb51a0d90c4522b3d8900c7b3
32BitFlame <47258322+32BitFlame@users.noreply.github.com>
2019-11-19 19:28:48 -0500

    Fixed bugs and introduced weapon
    * Fixed bug where bullets shot from the player hit the player
    * Introuced code for fusion rifle

--- starterGun_bullet.gd
+++ starterGun_bullet.gd
@@ -1,21 +1,30 @@
-extends KinematicBody2D
-const speed = 500;
+extends Node2D
+const speed = 2;
 var velocityVector;
 var started = false;
 var damage = 3;
-var origin = null;
-func StartMovement(x,y,tx, ty):
-	started = true
+var parent;
+func StartMovement(x,y,tx, ty, _parent):
+	parent = _parent;
 	self.position = Vector2(x,y);
+	self.rotation = parent.rotation;
 	velocityVector = Vector2(x - tx,y -ty);
 	velocityVector = velocityVector.normalized() * -speed;
+	started = true
+	print("here1");
 
 func _physics_process(delta):
 	if(!started):
 		return
-	move_and_collide(velocityVector)
+	self.translate(velocityVector)
 
 func _on_Area2D_body_entered(body):
-	if(body.has_method("damage") && body):
-		body.damage(damage);
-		
+	if(!started):
+		return
+	if(body == parent):
+		print(parent)
+		return;
+	else:
+		if(body.has_method("damage") && body):
+			body.damage(damage);
+	print("here3");
